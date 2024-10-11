dfbbb0528993adf39cb806b5aa2dfd0ec07b7b2f
Sponge <Jackp.bcot@gmail.com>
2024-10-11 09:38:01 +0100

    feat: added git nixos configuration to safe dir

--- git.nix
+++ git.nix
@@ -6,6 +6,7 @@
     enable = true;
     userName = "Sponge";
     userEmail = "Jackp.bcot@gmail.com";
+    extraConfig = { safe.directory = "/etc/nixos"; };
   };
   programs.bash.shellAliases = { lz = "lazygit"; };
 }
